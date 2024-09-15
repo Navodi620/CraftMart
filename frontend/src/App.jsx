1255b1374589d8a808a776a513325db72ba867c5
Navodi620 <navoditharaka8@gmail.com>
2024-09-15 16:58:04 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,30 @@ import CreatePayment from './pages/CreatePayments';
 import ShowPayment from './pages/ShowPayment';
 import EditPayment from './pages/EditPayment';
 import DeletePayment from './pages/DeletePayment';
+import FeedbackDashbord from './pages/FeedbackDashbord';
+import CreateFeedback from './pages/CreateFeedback';
+import DeleteFeedback from './pages/DeleteFeedback';
+import EditFeedback from './pages/EditFeedback';
+import FullFeedback from './pages/FullFeedback';
+import ReplyFeedback from './pages/ReplyFeedback';
+import ShowFeedback from './pages/ShowFeedback';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<PaymentDashbord />} />
+      <Route path='/paymenthome' element={<PaymentDashbord />} />
       <Route path='/payments/create' element={<CreatePayment />} />
       <Route path='/payments/details/:id' element={<ShowPayment />} />
       <Route path='/payments/edit/:id' element={<EditPayment />} />
       <Route path='/payments/delete/:id' element={<DeletePayment />} />
+
+      <Route path='/feedbackshome' element={<FeedbackDashbord />} />
+      <Route path='/feedbacks/create' element={<CreateFeedback />} />
+      <Route path='/feedbacks/delete/:id' element={<DeleteFeedback />} />
+      <Route path='/feedbacks/edit/:id' element={<EditFeedback />} />
+      <Route path='/feedbacks/full' element={<FullFeedback />} />
+      <Route path='/feedbacks/reply/:id' element={<ReplyFeedback />} />
+      <Route path='/feedbacks/details/:id' element={<ShowFeedback />} />
     </Routes>
   )
 }
